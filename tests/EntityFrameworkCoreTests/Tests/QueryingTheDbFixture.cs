006029a6180a5a579ac3d81d28a562461185056b
Majzur <nikodem.mazur@gmail.com>
2020-08-19 21:17:53 +0200

    Add a test method

--- tests/EntityFrameworkCoreTests/Tests/QueryingTheDbFixture.cs
+++ tests/EntityFrameworkCoreTests/Tests/QueryingTheDbFixture.cs
@@ -1,34 +1,39 @@
 ﻿using EntityFrameworkCoreTests.Context;
 using EntityFrameworkCoreTests.Db;
 using EntityFrameworkCoreTests.Logging;
-using System;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace EntityFrameworkCoreTests.Tests
 {
-    public class CrudFixture
+    public class QueryingTheDbFixture
     {
         private readonly ITestOutputHelper _testOutput;
 
-        public CrudFixture(ITestOutputHelper testOutputHelper)
+        public QueryingTheDbFixture(ITestOutputHelper testOutputHelper)
         {
             _testOutput = testOutputHelper;
         }
 
         [Fact]
-        public void FirstTest()
+        public void ReferencedCollectionWhenNotIncludedIsNull()
         {
             using var ctx =
                 SqlDb.CreateConnectionString()
                      .AsSqlConnectionString<BookStoreContext>()
                      .EnsureDb()
                      .SeedWith(@"TestData\RawTestData1.json")
-                     .ToDbContext()
+                     .BuildDbContext()
                      .StartLogging(_testOutput.AsLineWriter());
 
-            foreach (var book in ctx.Books)
-                _testOutput.WriteLine(new { book.Title, book.Price }.ToString());
+            ctx
+                .Books
+                .First()
+                .Reviews
+                .Should().BeNull("because the Reviews table to which the Books is linked has not been included by Include()");
         }
     }
 }
