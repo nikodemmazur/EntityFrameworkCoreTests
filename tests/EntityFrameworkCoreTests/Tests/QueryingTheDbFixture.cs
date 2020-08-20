048a9c384a0a8e23cf90c50103e873eb6434e9c2
Majzur <nikodem.mazur@gmail.com>
2020-08-20 22:02:40 +0200

    Place every method in a separate class to allow parallelism. Add test methods

--- tests/EntityFrameworkCoreTests/Tests/QueryingTheDbFixture.cs
+++ tests/EntityFrameworkCoreTests/Tests/QueryingTheDbFixture.cs
@@ -9,31 +9,99 @@ using Xunit.Abstractions;
 
 namespace EntityFrameworkCoreTests.Tests
 {
+    /// <summary>
+    /// Every test method is placed in a separate class to let Xunit run them in parallel.
+    /// </summary>
     public class QueryingTheDbFixture
     {
         private readonly ITestOutputHelper _testOutput;
 
-        public QueryingTheDbFixture(ITestOutputHelper testOutputHelper)
+        private BookStoreContext CreateBookStoreContext(string filePathToSeedDbWith = @"TestData\RawTestData1.json")
         {
-            _testOutput = testOutputHelper;
+            return SqlDb.CreateConnectionString(1)
+                        .AsSqlConnectionString<BookStoreContext>()
+                        .EnsureDb()
+                        .SeedWith(filePathToSeedDbWith)
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
         }
 
-        [Fact]
-        public void ReferencedCollectionWhenNotIncludedIsNull()
+        public QueryingTheDbFixture(ITestOutputHelper testOutput)
         {
-            using var ctx =
-                SqlDb.CreateConnectionString()
-                     .AsSqlConnectionString<BookStoreContext>()
-                     .EnsureDb()
-                     .SeedWith(@"TestData\RawTestData1.json")
-                     .BuildDbContext()
-                     .StartLogging(_testOutput.AsLineWriter());
+            _testOutput = testOutput;
+        }
 
-            ctx
-                .Books
-                .First()
-                .Reviews
-                .Should().BeNull("because the Reviews table to which the Books is linked has not been included by Include()");
+        public class BookCountShouldBeFourTest : QueryingTheDbFixture
+        {
+
+            public BookCountShouldBeFourTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+            [Fact]
+            public void BookCountShouldBeFour()
+            {
+                using var context = CreateBookStoreContext();
+
+                context
+                    .Books
+                    .Count()
+                    .Should().Be(4, "because the json test data has 4 items");
+            }
+        }
+
+        public class ReviewsShouldBeNullTest : QueryingTheDbFixture
+        {
+            public ReviewsShouldBeNullTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+            [Fact]
+            public void ReviewsShouldBeNull()
+            {
+                using var context = CreateBookStoreContext();
+
+                context
+                    .Books
+                    .First()
+                    .Reviews
+                    .Should().BeNull("because the Reviews table to which the Books is linked has not been included by Include()");
+            }
+        }
+
+        public class ReviewsShouldNotBeNullTest : QueryingTheDbFixture
+        {
+            public ReviewsShouldNotBeNullTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+            [Fact]
+            public void ReviewsShouldNotBeNull()
+            {
+                using var context = CreateBookStoreContext();
+
+                context
+                    .Books
+                    .Include(b => b.Reviews)
+                    .First()
+                    .Reviews
+                    .Should().NotBeNull("because Reviews have been included with Include()");
+            }
+        }
+
+        public class AuthorShouldNotBeNullTest : QueryingTheDbFixture
+        {
+            public AuthorShouldNotBeNullTest(ITestOutputHelper testOutput) : base(testOutput) { }
+
+            [Fact]
+            public void AuthorShouldNotBeNull()
+            {
+                using var context = CreateBookStoreContext();
+
+                context
+                    .Books
+                    .Include(b => b.AuthorsLink)
+                    .ThenInclude(al => al.Author)
+                    .First()
+                    .AuthorsLink
+                    .First()
+                    .Author
+                    .Should().NotBeNull("because Author has been included as a second-level relationship with ThenInclude()");
+            }
         }
     }
 }
