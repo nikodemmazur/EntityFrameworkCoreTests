38099aba8a1ac2fd6ec478c6e009e3af249c21b4
Majzur <nikodem.mazur@gmail.com>
2020-09-02 21:26:16 +0200

    Add new test class

---
+++ tests/EntityFrameworkCoreTests/Tests/ConfiguringRelationships.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkCoreTests.TestHelpers;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCoreTests.Tests
+{
+    public class ConfiguringRelationships : TestClassBase
+    {
+        private readonly ITestOutputHelper _testOutput;
+
+        public ConfiguringRelationships(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+
+
+    }
+}
