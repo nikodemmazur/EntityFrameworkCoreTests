29c1df422b8cf92aa1a92612bee77ed17f6c51f8
Majzur <nikodem.mazur@gmail.com>
2020-09-04 12:18:28 +0200

    Add one-to-one relationship tests

--- tests/EntityFrameworkCoreTests/Tests/ConfiguringRelationships.cs
+++ tests/EntityFrameworkCoreTests/Tests/ConfiguringRelationships.cs
@@ -1,17 +1,241 @@
-﻿using EntityFrameworkCoreTests.TestHelpers;
+﻿using EntityFrameworkCoreTests.Context;
+using EntityFrameworkCoreTests.Db;
+using EntityFrameworkCoreTests.Logging;
+using EntityFrameworkCoreTests.TestHelpers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Xunit;
 using Xunit.Abstractions;
+
+#nullable disable
 
 namespace EntityFrameworkCoreTests.Tests
 {
-    public class ConfiguringRelationships : TestClassBase
+    public class ConfiguringRelationships
     {
-        private readonly ITestOutputHelper _testOutput;
-
-        public ConfiguringRelationships(ITestOutputHelper testOutput)
+        /// <summary>
+        /// One-to-one relationship in which <see cref="Attendee"/> is dependent on <see cref="Ticket"/>.
+        /// Notice that "one-to-one" applies to <see cref="Attendee"/> as <see cref="Ticket"/> doesn't require <see cref="Attendee"/> to exist.
+        /// The manner in how <see cref="Ticket"/> is related to <see cref="Attendee"/> should be rather defined as "one-to-zero-or-one".
+        /// </summary>
+        public class OptionOne : TestClassBase
         {
-            _testOutput = testOutput;
+            private readonly ITestOutputHelper _testOutput;
+
+            public OptionOne(ITestOutputHelper testOutput)
+            {
+                _testOutput = testOutput;
+            }
+
+            public class Ticket
+            {
+                public int TicketId { get; set; }
+                public string Type { get; set; }
+            }
+
+            public class Attendee
+            {
+                public int AttendeeId { get; set; }
+                public string Name { get; set; }
+                public int ConventionTicketId { get; set; }
+            }
+
+            public class ConventionContext : DbContext
+            {
+                public DbSet<Attendee> Attendees { get; set; }
+
+                public ConventionContext(DbContextOptions<ConventionContext> options) : base(options) { }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    modelBuilder.Entity<Attendee>()
+                                .HasOne<Ticket>() // Required because Attendee doesn't have a navigation property to Ticket.
+                                .WithOne() // Required by HasOne() as a subsequent call.
+                                .HasForeignKey<Attendee>(a => a.ConventionTicketId); // Required because 'ConventionTicketId' doesn't meet 
+                                                                                     // EF Core convention (it's expected to be as simple as 'TicketId').
+                }
+            }
+
+            [Fact]
+            public void CreatesOptionOneRelationship()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<ConventionContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var newTicket = new Ticket { Type = "normal" };
+                context.Add(newTicket);
+                context.SaveChanges();
+                newTicket.TicketId.Should().NotBe(0, "because new id has been assigned by the DB");
+
+                var newAttendee = new Attendee { Name = "John Smith", ConventionTicketId = newTicket.TicketId };
+                context.Add(newAttendee);
+                context.SaveChanges();
+
+                // Inner join as an equivalent of EF Core Include() for an entity with no navigation property.
+                var nameTicketTypePair =
+                    context
+                        .Attendees
+                        .Join(context.Set<Ticket>(), a => a.ConventionTicketId, t => t.TicketId, (a, t) => new { a.Name, t.Type })
+                        .First();
+
+                nameTicketTypePair.Should().BeEquivalentTo(new { Name = "John Smith", Type = "normal" }, "because that's the expected output" +
+                    " of the above query");
+            }
         }
 
+        /// <summary>
+        /// This class differs from <see cref="OptionOne"/> in that it inverted the relationship and let entities have navigation properties.
+        /// </summary>
+        public class OptionTwo : TestClassBase
+        {
+            private readonly ITestOutputHelper _testOutput;
 
+            public OptionTwo(ITestOutputHelper testOutput)
+            {
+                _testOutput = testOutput;
+            }
+
+            public class Ticket
+            {
+                public int TicketId { get; set; }
+                public string Type { get; set; }
+                public int AttendeeId { get; set; }
+                public Attendee Attendee { get; set; }
+            }
+
+            public class Attendee
+            {
+                public int AttendeeId { get; set; }
+                public string Name { get; set; }
+                public Ticket Ticket { get; set; }
+            }
+
+            public class ConventionContext : DbContext
+            {
+                public DbSet<Attendee> Attendees { get; set; }
+
+                public ConventionContext(DbContextOptions<ConventionContext> options) : base(options) { }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    // Everything is handled by EF Core conventions.
+                }
+            }
+
+            [Fact]
+            public void CreatesOptionTwoRelationship()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<ConventionContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var newAttendee = new Attendee { Name = "John Smith" }; // Attendee can exist without Ticket.
+                context.Add(newAttendee); // var newAttendee is now being tracked.
+                context.SaveChanges();
+                newAttendee.Ticket.Should().BeNull($"because it has not been provided and {nameof(Attendee)} can exist without {nameof(Ticket)}");
+
+                newAttendee.Ticket = new Ticket { Type = "normal" }; // Properties Attendee, AttendeeId and TicketId are handled by EF Core.
+                context.SaveChanges();
+
+                var nameTicketTypePair =
+                    context
+                        .Attendees
+                        .Include(a => a.Ticket) // Now, Include() handles joining in the DB.
+                        .Select(a => new { a.Name, a.Ticket.Type }) // Much simpler than the one in OptionOne.
+                        .First();
+
+                nameTicketTypePair.Should().BeEquivalentTo(new { Name = "John Smith", Type = "normal" }, "because that's the expected output" +
+                    " of the above query");
+            }
+        }
+
+        /// <summary>
+        /// The recommended way to create dependent tables.
+        /// </summary>
+        public class OptionThree : TestClassBase
+        {
+            private readonly ITestOutputHelper _testOutput;
+
+            public OptionThree(ITestOutputHelper testOutput)
+            {
+                _testOutput = testOutput;
+            }
+
+            public class Ticket
+            {
+                public int TicketId { get; set; }
+                public string Type { get; set; }
+                public int AttendeeId { get; set; }
+                public Attendee Attendee { get; set; }
+            }
+
+            public class Attendee
+            {
+                public int AttendeeId { get; set; }
+                public string Name { get; set; }
+                public Ticket Ticket { get; set; }
+            }
+
+            public class ConventionContext : DbContext
+            {
+                public DbSet<Attendee> Attendees { get; set; }
+
+                public ConventionContext(DbContextOptions<ConventionContext> options) : base(options) { }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    modelBuilder.Entity<Ticket>()
+                                .HasKey(t => new { t.TicketId, t.AttendeeId }); // Required because not supported by conventions.
+                }
+            }
+
+            [Fact]
+            public void CreatesOptionThreeRelationship()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<ConventionContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var newAttendee = new Attendee { Name = "John Smith" }; // Attendee can exist without Ticket.
+                context.Add(newAttendee); // var newAttendee is now being tracked.
+                context.SaveChanges();
+                newAttendee.Ticket.Should().BeNull($"because it has not been provided and {nameof(Attendee)} can exist without {nameof(Ticket)}");
+
+                var newAttendeeEntry = context.Entry(newAttendee);
+
+                newAttendeeEntry
+                    .Invoking(ae => ae.Property($"{nameof(Attendee.Ticket)}{nameof(Ticket.TicketId)}").CurrentValue)
+                    .Should()
+                    .Throw<Exception>("because there's no need for a shadow property to determine the relationship by EF Core");
+
+                newAttendee.Ticket = new Ticket { Type = "normal" }; // Properties Attendee, AttendeeId and TicketId are handled by EF Core.
+                context.SaveChanges();
+
+                var nameTicketTypePair =
+                    context
+                        .Attendees
+                        .Include(a => a.Ticket)
+                        .Select(a => new { a.Name, a.Ticket.Type })
+                        .First();
+
+                nameTicketTypePair.Should().BeEquivalentTo(new { Name = "John Smith", Type = "normal" }, "because that's the expected output" +
+                    " of the above query");
+            }
+        }
     }
 }
