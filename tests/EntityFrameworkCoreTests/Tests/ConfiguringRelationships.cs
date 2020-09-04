b5e73541a075289fba7e612b4d862af63bb471b0
Majzur <nikodem.mazur@gmail.com>
2020-09-04 22:47:16 +0200

    Add many-to-many relationship and alternate unique key tests

--- tests/EntityFrameworkCoreTests/Tests/ConfiguringRelationships.cs
+++ tests/EntityFrameworkCoreTests/Tests/ConfiguringRelationships.cs
@@ -273,8 +273,187 @@ namespace EntityFrameworkCoreTests.Tests
                     modelBuilder.Entity<Notebook>()
                                 .HasMany(n => n.Usbs)
                                 .WithOne()
-                                .HasForeignKey(u => u.HostId);
+                                .HasForeignKey(u => u.HostId); // Required because 'HostId' doesn't meet the naming convention.
                 }
+            }
+
+            [Fact]
+            public void CreatesOneToManyRelationship()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<ManufacturerContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var newUsbs = new[] { new UsbSlot { UsbVersion = "3.0" }, new UsbSlot { UsbVersion = "2.0" } };
+                var newNotebook = new Notebook { Name = "Pear", Usbs = newUsbs };
+                context.Add(newNotebook);
+                context.SaveChanges();
+
+                context
+                    .Notebooks
+                    .Include(n => n.Usbs)
+                    .SingleOrDefault()
+                    .Should().NotBeNull("because has been successfully added");
+            }
+        }
+
+        public class ManyToManyByConvention : TestClassBase
+        {
+            private readonly ITestOutputHelper _testOutput;
+
+            public ManyToManyByConvention(ITestOutputHelper testOutput)
+            {
+                _testOutput = testOutput;
+            }
+
+            public class Book
+            {
+                public int BookId { get; set; }
+                public string Title { get; set; }
+                public ICollection<BookAuthor> AuthorLinks { get; set; }
+            }
+
+            public class Author
+            {
+                public int AuthorId { get; set; }
+                public string Name { get; set; }
+                public ICollection<BookAuthor> BookLinks { get; set; }
+            }
+
+            public class BookAuthor
+            {
+                public int BookId { get; set; }
+                public int AuthorId { get; set; }
+                public Book Book { get; set; }
+                public Author Author { get; set; }
+            }
+
+            public class BookStoreContext : DbContext
+            {
+                public DbSet<Book> Books { get; set; }
+
+                public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    modelBuilder
+                        .Entity<BookAuthor>()
+                        .HasKey(ba => new { ba.BookId, ba.AuthorId });
+                }
+            }
+
+            [Fact]
+            public void CreatesManyToManyRelationshipByConvention()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<BookStoreContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var aFreemanAuthor = new Author { Name = "Adam Freeman" };
+                var eEvansAuthor = new Author { Name = "Eric Evans" };
+                var aspBook = new Book { Title = "...ASP.NET CORE..." };
+                var dddBook = new Book { Title = "...DDD..." };
+                context
+                    .Invoking(ctx =>
+                        {
+                            ctx.AddRange(new[] { aFreemanAuthor, eEvansAuthor });
+                            ctx.AddRange(new[] { aspBook, dddBook });
+                            ctx.SaveChanges();
+                        })
+                    .Should().NotThrow("because it's actually the 'zero-or-one-or-many' relationship.");
+                context.Entry(aspBook).State.Should().Be(EntityState.Unchanged, "because it has already been saved and" +
+                    " has not been modified afterwards");
+                aspBook.AuthorLinks.Should().BeNull("because tracking doesn't automatically loads navigation properties");
+                context.Entry(aspBook).Collection(a => a.AuthorLinks).Load(); // Load navigation property explicitly.
+                context.Entry(aspBook).State.Should().Be(EntityState.Unchanged, "because it's still unchanged - just its property has been loaded");
+                aspBook.AuthorLinks.Should().NotBeNull("because already loaded");
+                aspBook.AuthorLinks.Count().Should().Be(0, "because no links have been specified");
+                aspBook.AuthorLinks = aspBook.AuthorLinks.Append(new BookAuthor { Author = aFreemanAuthor }).ToList();
+                context.Invoking(ctx => ctx.SaveChanges()).Should().NotThrow("because just Author must be specified in BookAuthor - the rest is" +
+                    " handled by EF Core");
+                aspBook.AuthorLinks = aspBook.AuthorLinks.Append(new BookAuthor { Author = eEvansAuthor }).ToList();
+                dddBook.AuthorLinks = new List<BookAuthor> { new BookAuthor { Author = eEvansAuthor }, new BookAuthor { Author = aFreemanAuthor } };
+                context.SaveChanges();
+                context.Set<BookAuthor>().Count().Should().Be(4, "because 2 authors * 2 books");
+            }
+        }
+
+        public class AlternateUniqueKey : TestClassBase
+        {
+            private readonly ITestOutputHelper _testOutput;
+
+            public AlternateUniqueKey(ITestOutputHelper testOutput)
+            {
+                _testOutput = testOutput;
+            }
+
+            public class Person
+            {
+                public int PersonId { get; set; }
+                public string Name { get; set; }
+                public string UserId { get; set; } // which is the person's email address and therefore unique
+                public ContactInfo Contact { get; set; }
+            }
+
+            public class ContactInfo
+            {
+                public int ContactInfoId { get; set; }
+                public string MobileNumber { get; set; }
+                public string EmailAddress { get; set; }
+            }
+
+            public class ContactBookContext : DbContext
+            {
+                public DbSet<Person> People { get; set; }
+
+                public ContactBookContext(DbContextOptions<ContactBookContext> options) : base(options) { }
+
+                protected override void OnModelCreating(ModelBuilder modelBuilder)
+                {
+                    //modelBuilder.Entity<Person>()
+                                //.HasAlternateKey(p => p.UserId); // no need to do that because of the below HasPrincipalKey().
+
+                    modelBuilder.Entity<Person>()
+                                .Property(p => p.UserId)
+                                .IsRequired();
+
+                    modelBuilder.Entity<ContactInfo>()
+                                .Property(ci => ci.EmailAddress)
+                                .IsRequired();
+
+                    modelBuilder.Entity<Person>()
+                                .HasOne(p => p.Contact)
+                                .WithOne()
+                                .HasForeignKey<ContactInfo>(ci => ci.EmailAddress)
+                                .HasPrincipalKey<Person>(p => p.UserId);
+                }
+            }
+
+            [Fact]
+            public void CreatesRelationshipUsingAlternateUniqueKey()
+            {
+                var dbConnectionString = DbConnectionString.Create(nameof(ConfiguringRelationships), GetCallerName());
+                using var context =
+                    dbConnectionString
+                        .AsSqlConnectionString<ContactBookContext>()
+                        .EnsureDb()
+                        .BuildDbContext()
+                        .StartLogging(_testOutput.AsLineWriter());
+
+                var newContact = new ContactInfo { MobileNumber = "1234567890" };
+                var newPerson = new Person { Name = "John Smith", UserId = "jsmith@gmail.com", Contact = newContact };
+                context.Add(newPerson);
+                context.SaveChanges();
+                newContact.EmailAddress.Should().Be(newPerson.UserId, "because it's the foreign key linking to Person by its UserId" +
+                    " which has already been set by EF Core on SaveChanges()");
             }
         }
     }
